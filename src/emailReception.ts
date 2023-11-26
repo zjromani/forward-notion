0a8fc48b4ad8cc42d5d481568c152f63002b17e6
Zach Romani <zach.romani@hotelengine.com>
2023-11-25 19:36:40 -0700

    Removed cb for promis

--- src/emailReception.ts
+++ src/emailReception.ts
@@ -30,36 +30,54 @@ const imapConfig: ImapConfig = imapConfigSchema.parse(config);
 
 const imap = new Imap(imapConfig);
 
-function openInbox(cb: (err: Error, mailbox: Connection.Box) => void): void {
+function openInbox(cb: (err: Error, mailbox: Imap.Box) => void): void {
   imap.openBox('INBOX', false, cb);
 }
 
-imap.once('ready', () => {
-  openInbox((err: Error, mailbox: Connection.Box) => {
-    if (err) throw err;
-    const searchCriteria = ['UNSEEN'];
-    const fetchOptions: Imap.FetchOptions = { bodies: ['HEADER', 'TEXT'], markSeen: true };
+export function fetchUnseenEmails(): Promise<ParsedMail[]> {
+  return new Promise((resolve, reject) => {
+    imap.once('ready', () => {
+      openInbox((err: Error, mailbox: Imap.Box) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        const searchCriteria = ['UNSEEN'];
+        const fetchOptions: Imap.FetchOptions = { bodies: ['HEADER', 'TEXT'], markSeen: true };
 
-    imap.search(searchCriteria, (searchErr: Error, results: number[]) => {
-      if (searchErr) throw searchErr;
-      const fetch = imap.fetch(results, fetchOptions);
+        imap.search(searchCriteria, (searchErr: Error, results: number[]) => {
+          if (searchErr) {
+            reject(searchErr);
+            return;
+          }
+          const fetchedEmails: ParsedMail[] = [];
+          const fetch = imap.fetch(results, fetchOptions);
 
-      fetch.on('message', (msg: Imap.ImapMessage, seqno: number) => {
-        msg.on('body', (stream: Source) => {
-          simpleParser(stream, (parseErr: Error, mail: ParsedMail) => {
+          fetch.on('message', (msg: Imap.ImapMessage, seqno: number) => {
+            msg.on('body', (stream: Source) => {
+              simpleParser(stream, (parseErr: Error, mail: ParsedMail) => {
+                if (parseErr) {
+                  reject(parseErr);
+                  return;
+                }
+                fetchedEmails.push(mail);
+              });
+            });
+          });
+
+          fetch.on('end', () => {
+            imap.end();
+            resolve(fetchedEmails);
           });
         });
       });
-
-      fetch.on('end', () => {
-        imap.end();
-      });
     });
+
+    imap.once('error', (err: Error) => {
+      console.error(err);
+      reject(err);
+    });
+
+    imap.connect();
   });
-});
-
-imap.once('error', (err: Error) => {
-  console.error(err);
-});
-
-imap.connect();
+}
