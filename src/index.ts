a9732f7d9c3633c92fdc7f38751192f461401441
Zach Romani <zach.romani@hotelengine.com>
2023-12-12 21:23:50 -0700

    Updated for handler
    Seeing if we can get this thing shoved in a lambda and test with event
    bridge trigger

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,26 @@
+import { Handler } from "aws-lambda";
 import { fetchUnseenEmails } from "./emailReception";
 import { sendToNotion } from "./notion/sendToNotion";
 import { processEmailContent } from "./notion/transformData";
 
-fetchUnseenEmails()
-  .then(emails => {
-    return Promise.all(emails.map(email => processEmailContent(email)));
-  })
-  .then(parsedEmails => {
-    const email = parsedEmails[0];
-    // return Promise.all(
-    //   parsedEmails.map(parsedEmail => sendToNotion(parsedEmail))
-    // );
+export const handler: Handler = async (): Promise<any> => {
+  fetchUnseenEmails()
+    .then(emails => {
+      return Promise.all(emails.map(email => processEmailContent(email)));
+    })
+    .then(parsedEmails => {
+      const email = parsedEmails[0];
+      // return Promise.all(
+      //   parsedEmails.map(parsedEmail => sendToNotion(parsedEmail))
+      // );
 
-    // console.log(email);
-    return sendToNotion(email);
-  })
-  .then(results => {
-    console.log("All emails have been sent to Notion:", results);
-  })
-  .catch(error => {
-    console.error("An error occurred:", error);
-  });
+      // console.log(email);
+      return sendToNotion(email);
+    })
+    .then(results => {
+      console.log("All emails have been sent to Notion:", results);
+    })
+    .catch(error => {
+      console.error("An error occurred:", error);
+    });
+};
